ab578ecc6814974561c6788ffe4e534e70b98bee
Barrie Freyre <barriedirk@yahoo.com>
2025-05-16 21:56:43 -0500

    add js

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
 const $ = (selector) => document.querySelector(selector);
+const $$ = (selector) => document.querySelectorAll(selector);
 
 (async () => {
   const $btnNavToggle = $("#menu__toggle");
-  const $headerMenu = $("header.menu");
+  const $body = $("body");
 
   $btnNavToggle.addEventListener("click", async (evt) => {
     evt.preventDefault();
@@ -11,10 +12,75 @@ const $ = (selector) => document.querySelector(selector);
 
     if (isExpanded) {
       $btnNavToggle.setAttribute("aria-expanded", "false");
-      $headerMenu.classList.remove("is-expanded");
+      $body.classList.remove("is-expanded");
     } else {
       $btnNavToggle.setAttribute("aria-expanded", "true");
-      $headerMenu.classList.add("is-expanded");
+      $body.classList.add("is-expanded");
     }
   });
+
+  // slider
+  let galleryIdx = 0;
+  let galleryTimeout = 0;
+
+  const $galleryBtnPrev = $("button.hero-image__navigation-btn--prev");
+  const $galleryBtnNext = $("button.hero-image__navigation-btn--next");
+
+  const $$galleryPictures = $$(".hero-image__item");
+  const $$galleryDestails = $$(".hero__details--item");
+
+  const galleryLength = $$galleryPictures.length;
+
+  const GALLERY_DIRECTION = {
+    PREV: -1,
+    NEXT: 1,
+  };
+
+  const changePictureAutomatically = () => {
+    clearTimeout(galleryTimeout);
+
+    galleryTimeout = setTimeout(() => {
+      galleryDirection(GALLERY_DIRECTION.NEXT);
+
+      changePictureAutomatically();
+    }, 3000);
+  };
+
+  const galleryDirection = (direction) => {
+    clearTimeout(galleryTimeout);
+    $$galleryPictures[galleryIdx].classList.add("d-none");
+    $$galleryDestails[galleryIdx].classList.add("d-none");
+    $$galleryPictures[galleryIdx].classList.remove("d-flex");
+    $$galleryDestails[galleryIdx].classList.remove("d-flex");
+
+    const newIdx = galleryIdx + direction;
+
+    if (newIdx < 0) {
+      galleryIdx = galleryLength - 1;
+    } else if (newIdx > galleryLength - 1) {
+      galleryIdx = 0;
+    } else {
+      galleryIdx = newIdx;
+    }
+
+    $$galleryPictures[galleryIdx].classList.remove("d-none");
+    $$galleryDestails[galleryIdx].classList.remove("d-none");
+    $$galleryPictures[galleryIdx].classList.add("d-flex");
+    $$galleryDestails[galleryIdx].classList.add("d-flex");
+
+    changePictureAutomatically();
+  };
+
+  $galleryBtnPrev.addEventListener("click", (evt) => {
+    evt.preventDefault();
+
+    galleryDirection(GALLERY_DIRECTION.PREV);
+  });
+  $galleryBtnNext.addEventListener("click", (evt) => {
+    evt.preventDefault();
+
+    galleryDirection(GALLERY_DIRECTION.NEXT);
+  });
+
+  changePictureAutomatically();
 })();
